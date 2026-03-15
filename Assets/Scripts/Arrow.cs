6668a1ccdce74f66d05901314669b92fb8e34afd
Elkwizard <bananamanthefourth@gmail.com>
2026-03-15 13:55:58 -0700

    added bow and arrow, sticks in walls

---
+++ Assets/Scripts/Arrow.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    public Player player;
+    private bool hit;
+    private Rigidbody rb;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        hit = false;
+        rb = GetComponent<Rigidbody>();
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player.gameObject)
+            return;
+
+        if (other.gameObject.GetComponent<Arrow>() != null)
+            return;
+
+        if (!hit)
+        {
+            hit = true;
+            rb.isKinematic = true;
+            DestructionManager.GetInstance().Break(new(rb.worldCenterOfMass, player.arrowPower));
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (!hit)
+        {
+            transform.rotation = Quaternion.LookRotation(rb.linearVelocity);
+        }
+    }
+}
